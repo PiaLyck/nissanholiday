2f73def579a0d38d3237f17089ca40d46f862e9d
Pia Lyck Festersen <PiaLyck.Festersen@akqa.com>
2017-09-25 10:47:28 +1000

    add dist files

---
+++ dist/scripts/main.js
@@ -0,0 +1,1 @@
+"use strict";$("#title-dropdown").selectize({create:!0,sortField:"text"}),$(".unicode-part").keyup(function(){this.value.length==this.maxLength&&$(this).next(".unicode-part").focus()});
